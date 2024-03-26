e53c7d9cf809a644b2248aa92ead282fb4dfff62
ClaireChilders <clairechilders@oakland.edu>
2024-03-26 17:13:56 -0400

    feat: add additional feature cleaning

--- preprocessing.py
+++ preprocessing.py
@@ -12,7 +12,7 @@ def load_data():
 
     appointment_df = pd.read_csv('data/appointment_data.csv')
     attendance_df = pd.read_csv('data/attendance_data.csv')
-    # career_fair_df = pd.read_csv('data/career_fair_data.csv')
+    career_fair_df = pd.read_csv('data/career_fair_data.csv')
     registration_df = pd.read_csv('data/registration_data.csv')
     student_df = pd.read_csv('data/student_data.csv')
 
@@ -33,10 +33,10 @@ def load_data():
 
     print(Fore.GREEN + "Data merged" + Style.RESET_ALL)
 
-    return merged_data
+    return merged_data, career_fair_df
 
 
-def clean_data(data):
+def clean_data(data, career_fair_df):
     print(Fore.MAGENTA + "\nCleaning data..." + Style.RESET_ALL)
 
     # Convert Yes/No to 1/0
@@ -76,6 +76,139 @@ def clean_data(data):
         data['stu_grad_date']).dt.year
 
     print(Fore.BLUE + "  Dates converted to year only" + Style.RESET_ALL)
+
+    # ===============================================================
+    #                       Appointments Cleaning
+    # ===============================================================
+
+    # Convert appointment list to a count of different categories of
+    #   appointments including: Walk-Ins, Resume Reviews, Career Fair
+    #   Preparation, Career Exploration, Internship/Job Search, and
+    #   Other. Also, drop the original appointment_types column since
+    #   we only need the counts.
+
+    print(Fore.BLUE + "  Extracting appointment types..." + Style.RESET_ALL)
+
+    # First ensure that appointment_types only contains lists
+
+    data['appointment_types'] = data['appointment_types'].apply(
+        lambda x: x.split(',') if isinstance(x, str) else x
+    )
+    data['appointment_types'] = data['appointment_types'].apply(
+        lambda x: x if isinstance(x, list) else []
+    )
+
+    # Walk-Ins
+    walk_ins = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'walk-in' in i]
+    )
+    not_walk_ins = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'walk-in' not in i]
+    )
+    data['walk_in_count'] = len(walk_ins)
+    data['appointment_types'] = not_walk_ins
+
+    print(Fore.BLUE + "    Walk-Ins extracted" + Style.RESET_ALL)
+
+    # Resume Reviews
+    resume_reviews = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'resume' in i]
+    )
+    not_resume_reviews = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'resume' not in i]
+    )
+    data['resume_review_count'] = len(resume_reviews)
+    data['appointment_types'] = not_resume_reviews
+
+    print(Fore.BLUE + "    Resume Reviews extracted" + Style.RESET_ALL)
+
+    # Career Fair Prep
+    cf_prep = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'career fair' in i]
+    )
+    not_cf_prep = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'career fair' not in i]
+    )
+    data['cf_prep_count'] = len(cf_prep)
+    data['appointment_types'] = not_cf_prep
+
+    print(Fore.BLUE + "    Career Fair Prep extracted" + Style.RESET_ALL)
+
+    # Career Exploration
+    career_exploration = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'career exploration' in i]
+    )
+    not_career_exploration = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'career exploration' not in i]
+    )
+    data['career_exploration_count'] = len(career_exploration)
+    data['appointment_types'] = not_career_exploration
+
+    print(Fore.BLUE + "    Career Exploration extracted" + Style.RESET_ALL)
+
+    # Internship/Job Search
+    job_search = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'internship' in i or 'job' in i]
+    )
+    not_job_search = data['appointment_types'].apply(
+        lambda x: [i for i in x if 'internship' not in i and 'job' not in i]
+    )
+    data['job_search'] = len(job_search)
+    data['appointment_types'] = not_job_search
+
+    print(Fore.BLUE + "    Internship/Job Search extracted" + Style.RESET_ALL)
+
+    # Other
+    data['other_count'] = data['appointment_types'].apply(
+        lambda x: len(x)
+    )
+
+    print(Fore.BLUE + "    Other extracted" + Style.RESET_ALL)
+
+    # Drop appointment_types since we've extracted the counts
+    data.drop(['appointment_types'], axis=1, inplace=True)
+
+    print(Fore.BLUE + "    Appointment types dropped" + Style.RESET_ALL)
+    print(Fore.GREEN + "  Appointments cleaned" + Style.RESET_ALL)
+
+    # ===============================================================
+    #                        Majors Cleaning
+    # ===============================================================
+
+    # Add a column for whether the student has a major that is
+    #   included in the career fair they attended
+
+    print(Fore.BLUE + "  Extracting student majors..." + Style.RESET_ALL)
+
+    # First ensure that stu_majors only contains lists
+
+    data['stu_majors'] = data['stu_majors'].apply(
+        lambda x: x.split(',') if isinstance(x, str) else x
+    )
+    data['stu_majors'] = data['stu_majors'].apply(
+        lambda x: x if isinstance(x, list) else []
+    )
+
+    # Check if any of the student's majors are included in the career fair's
+
+    data['has_major'] = data['stu_majors'].apply(
+        lambda x: any(
+            [major in career_fair_df['majors'].values for major in x])
+    )
+
+    # Convert majors back to strings for dummies
+    data['stu_majors'] = data['stu_majors'].apply(
+        lambda x: ', '.join(x) if isinstance(x, list) else x
+    )
+
+    print(Fore.GREEN + "  Majors cleaned" + Style.RESET_ALL)
+
+    # ===============================================================
+    #                          Null Values
+    # ===============================================================
+
+    # Fill null values with 0
+    data.fillna(0, inplace=True)
 
     print(Fore.GREEN + "Data cleaned" + Style.RESET_ALL)
 
